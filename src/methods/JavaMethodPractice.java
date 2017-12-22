21ca6c827321dd86cee7528882ec6f55d7a4731e
Moniruzzaman Monir <moniruzzaman000@gmail.com>
2017-12-22 15:22:35 -0500

    add method

--- src/methods/JavaMethodPractice.java
+++ src/methods/JavaMethodPractice.java
@@ -3,8 +3,24 @@ package methods;
 public class JavaMethodPractice {
 
 	public static void main(String[] args) {
-		
 
+		System.out.println(getCountOfAllChar("Moniruzzaman"));
+
+	}
+
+	/**
+	 * Exercise: 30 Write a method that take one String input and return the
+	 * count of all characters without whitespace. Parameter: String text
+	 * ReturnType: int ServingContainer: Variable
+	 */
+
+	public static int getCountOfAllChar(String text) {
+		int count = 0;
+		text = text.replace(" ", "");
+
+		count = text.length();
+
+		return count;
 	}
 
 }
